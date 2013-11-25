4389a020047228bde61ea048935318cdbbf8a83b
Markus Klemm <markus@markusklemm.net>
2013-11-25 11:21:07 +0100

    Initial Commit

---
+++ Bitkette/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Bitkette
+//
+//  Created by Markus Klemm on 25.11.13.
+//  Copyright (c) 2013 MK-Enterprises. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+

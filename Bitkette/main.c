d9695d98ce2cd465c427856030b9a419a507a558
Markus Klemm <markus@markusklemm.net>
2013-12-02 17:26:26 +0100

    Funktioniert mich Zahl verschieben.

--- Bitkette/main.c
+++ Bitkette/main.c
@@ -7,12 +7,46 @@
 //
 
 #include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+
+
+char* crtBitStr(unsigned int x);
 
 int main(int argc, const char * argv[])
 {
 
-    // insert code here...
-    printf("Hello, World!\n");
+    printf("Eingabe einer int-Zahl: ");
+    char cbuff[128];
+    
+    fgets(cbuff, 128, stdin);
+    
+    int ibuff = atoi(cbuff);
+    
+    printf("\n*** \ndez: %d \nhex %x\n",ibuff,ibuff);
+    
+    printf("bin: %s \n",crtBitStr(ibuff));
+    
+    printf("xxx\n");
+    
     return 0;
+}
+
+char* crtBitStr(unsigned int x)
+{
+    static char cresult[sizeof(x)*8+1];
+    
+    int mask = 1;
+    
+    int i;
+    
+    for (i=0; i<sizeof(x)*8; i++) {
+        cresult[i] = (x & mask)+48;
+        x>>=1;
+    }
+    
+    
+    return cresult;
+    
 }
 
